37200f6e6a4b5968029ed03cfb0bfb0c74532ec5
nurealam <nur42658@gmail.com>
2021-08-27 15:44:23 +0700

    navbar done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 import { Navbar } from "./Components";
 import { GlobalStyle } from "./Global.style";
 
 function App() {
   return (
-    <>
+    <Router>
       <GlobalStyle />
       <Navbar />
-    </>
+    </Router>
   );
 }
 
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,12 +1,84 @@
-import React from "react";
-import { Nav, NavbarContainer } from "./Navbar.elements";
+import React, { useEffect, useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import { IconContext } from "react-icons/lib";
+import { Button } from "../../Global.style";
+import {
+  MobileIcon,
+  Nav,
+  NavbarContainer,
+  NavBtnLink,
+  NavIcon,
+  NavItem,
+  NavItemBtn,
+  NavLinks,
+  NavLogo,
+  NavMenu,
+} from "./Navbar.elements";
 
 const Navbar = () => {
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
+
+  const handleClick = () => setClick(!click);
+
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  useEffect(() => {
+    showButton();
+  }, []);
+
+  window.addEventListener("resize", showButton);
+
   return (
     <>
-      <Nav>
-        <NavbarContainer></NavbarContainer>
-      </Nav>
+      <IconContext.Provider value={{ color: "#fff" }}>
+        <Nav>
+          <NavbarContainer>
+            <NavLogo to="/">
+              <NavIcon />
+              ULTEA
+            </NavLogo>
+            {/* mobile icon */}
+            <MobileIcon onClick={handleClick}>
+              {click ? <FaTimes /> : <FaBars />}
+            </MobileIcon>
+            {/* nav menu part */}
+            <NavMenu onClick={handleClick} click={click}>
+              <NavItem>
+                <NavLinks to="/">Home</NavLinks>
+              </NavItem>
+              <NavItem>
+                <NavLinks to="/">Services</NavLinks>
+              </NavItem>
+              <NavItem>
+                <NavLinks to="/">Products</NavLinks>
+              </NavItem>
+              <NavItem>
+                <NavLinks to="/">Services</NavLinks>
+              </NavItem>
+              <NavItemBtn>
+                {button ? (
+                  <NavBtnLink to="/sign-up">
+                    <Button primary>SIGN UP</Button>
+                  </NavBtnLink>
+                ) : (
+                  <NavBtnLink to="/sign-up">
+                    <Button fontBig primary>
+                      SIGN UP
+                    </Button>
+                  </NavBtnLink>
+                )}
+              </NavItemBtn>
+            </NavMenu>
+          </NavbarContainer>
+        </Nav>
+      </IconContext.Provider>
     </>
   );
 };
